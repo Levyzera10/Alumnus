dcff724325ced88c1d6041b5404bf7b28402f591
levyzera <levy.edu.11@gmail.com>
2022-04-08 05:10:12 -0300

    Classe Aluno

---
+++ src/alumnus/dominio/Pessoa.java
@@ -0,0 +1,33 @@
+package alumnus.dominio;
+
+public class Pessoa {
+    private String nome = "joão";
+    private int idade;
+    private double altura;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = 10;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
+
+
--- src/alumnus/teste/Menu.java
+++ src/alumnus/teste/Menu.java
@@ -1,7 +1,17 @@
 package alumnus.teste;
 
+import alumnus.dominio.Pessoa;
+
 public class Menu {
     public static void main(String[] args) {
 
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Bob esponja");
+        pessoa.setAltura(1.9);
+        pessoa.setIdade(20);
+
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+        System.out.println(pessoa.getAltura());
     }
 }
