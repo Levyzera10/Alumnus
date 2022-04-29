f3b443dbb6580b7d38800a6cefb0ca4609f22492
levyzera <levy.edu.11@gmail.com>
2022-04-28 21:30:52 -0300

    alterações classe Professor

---
+++ src/alumnus/dominio/Aula.java
@@ -0,0 +1,8 @@
+package alumnus.dominio;
+
+import java.time.LocalDateTime;
+
+public class Aula {
+    LocalDateTime horario;
+    Professor professor;
+}
--- src/alumnus/dominio/Professor.java
+++ src/alumnus/dominio/Professor.java
@@ -1,7 +1,25 @@
 package alumnus.dominio;
 
+import alumnus.enums.TurnoEnum;
+
 public class Professor extends Pessoa {
 
-    String matricula;
-    String turno;
+    private String matricula;
+    private TurnoEnum turno;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public TurnoEnum getTurno() {
+        return turno;
+    }
+
+    public void setTurno(TurnoEnum turno){
+        this.turno = turno;
+    }
 }
