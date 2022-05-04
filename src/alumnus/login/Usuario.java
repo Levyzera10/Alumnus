448b4824109bec28ee090c0eebf443b8609433bf
levyzera <levy.edu.11@gmail.com>
2022-05-04 00:42:04 -0300

    Alterações no login

--- src/alumnus/login/Usuario.java
+++ src/alumnus/login/Usuario.java
@@ -1,5 +1,14 @@
 package alumnus.login;
 
 public class Usuario {
+    String nome;
+    String senha;
+    String dataDeNascimento;
+
+    public Usuario(String nome , String senha ,String dataDeNascimento){
+        this.nome = nome;
+        this.senha = senha;
+        this.dataDeNascimento = dataDeNascimento;
+    }
 
 }
